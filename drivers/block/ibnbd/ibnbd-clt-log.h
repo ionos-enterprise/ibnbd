0ef50552b7fb631653249195cf4de3e248cec4a1
Roman Pen <roman.penyaev@profitbricks.com>
2017-06-15 14:47:11 +0200

    IBNBD: make log suitable for clt and srv

--- drivers/block/ibnbd/ibnbd-clt-log.h
+++ drivers/block/ibnbd/ibnbd-clt-log.h
@@ -1,34 +1,33 @@
 #ifndef __IBNBD_CLT_LOG_H__
 #define __IBNBD_CLT_LOG_H__
 
-#define blkdev_name(dev) ((dev->gd == NULL) ? "<no dev>" : dev->gd->disk_name)
-#define ibnbd_prefix_clt(dev) ((dev->sess->hostname[0] != '\0') ?		\
-			       dev->sess->hostname : dev->sess->str_addr)
+#define ibnbd_diskname(dev) ((dev)->gd ? (dev)->gd->disk_name : "<no dev>")
+#define ibnbd_prefix(dev) ((dev)->sess->hostname[0] ? (dev)->sess->hostname : \
+			   (dev)->sess->str_addr)
 
+#define ibnbd_log(lvl, fn, dev, fmt, ...) ({				\
+	if (__builtin_types_compatible_p(typeof(dev),			\
+					 struct ibnbd_dev *))		\
+		fn(lvl "<%s@%s> %s: " fmt, (dev)->pathname,		\
+		   ibnbd_prefix(dev), ibnbd_diskname(dev),		\
+		   ##__VA_ARGS__);					\
+	else if (__builtin_types_compatible_p(typeof(dev),		\
+					      struct ibnbd_srv_dev *))	\
+		fn(lvl "<%s@%s>: " fmt, (dev)->pathname,		\
+		   ibnbd_prefix(dev), ##__VA_ARGS__);			\
+})
 
-#define ERR(dev, fmt, ...) pr_err("<%s@%s> %s: " fmt,\
-				  dev->pathname, ibnbd_prefix_clt(dev),	\
-				  blkdev_name(dev), ##__VA_ARGS__)
-
-#define ERR_RL(dev, fmt, ...) pr_err_ratelimited("<%s@%s> %s: "\
-				fmt, dev->pathname,\
-				ibnbd_prefix_clt(dev), blkdev_name(dev),\
-				##__VA_ARGS__)
-
-#define WRN(dev, fmt, ...) pr_warn("<%s@%s> %s: " fmt,\
-				dev->pathname, ibnbd_prefix_clt(dev),\
-				blkdev_name(dev), ##__VA_ARGS__)
-
-#define WRN_RL(dev, fmt, ...) pr_warn_ratelimited("<%s@%s> %s: "\
-			fmt, dev->pathname, ibnbd_prefix_clt(dev),\
-			blkdev_name(dev), ##__VA_ARGS__)
-
-#define INFO(dev, fmt, ...) pr_info("<%s@%s> %s: " \
-			fmt, dev->pathname, ibnbd_prefix_clt(dev),\
-			blkdev_name(dev), ##__VA_ARGS__)
-
-#define INFO_RL(dev, fmt, ...) pr_info_ratelimited("<%s@%s> %s: " \
-			fmt, dev->pathname, ibnbd_prefix_clt(dev),\
-			blkdev_name(dev), ##__VA_ARGS__)
+#define ERR(dev, fmt, ...)	\
+	ibnbd_log(KERN_ERR, printk, dev, fmt, ##__VA_ARGS__)
+#define ERR_RL(dev, fmt, ...)	\
+	ibnbd_log(KERN_ERR, printk_ratelimited, dev, fmt, ##__VA_ARGS__)
+#define WRN(dev, fmt, ...)	\
+	ibnbd_log(KERN_WARNING, printk, dev, fmt, ##__VA_ARGS__)
+#define WRN_RL(dev, fmt, ...) \
+	ibnbd_log(KERN_WARNING, printk_ratelimited, dev, fmt, ##__VA_ARGS__)
+#define INFO(dev, fmt, ...) \
+	ibnbd_log(KERN_INFO, printk, dev, fmt, ##__VA_ARGS__)
+#define INFO_RL(dev, fmt, ...) \
+	ibnbd_log(KERN_INFO, printk_ratelimited, dev, fmt, ##__VA_ARGS__)
 
 #endif /*__IBNBD_CLT_LOG_H__*/
