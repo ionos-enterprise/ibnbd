7a8e19f9e17158c28950fc67b8f4315418f11627
Roman Pen <roman.penyaev@profitbricks.com>
2017-10-23 14:33:50 +0200

    IBTRS,IBNBD: remove workaround for fixed bug inside sockaddr_to_str()

--- drivers/infiniband/ulp/ibtrs/ibtrs-pri.h
+++ drivers/infiniband/ulp/ibtrs/ibtrs-pri.h
@@ -311,21 +311,6 @@ static inline void sockaddr_to_str(const struct sockaddr_storage *addr,
 			  &((struct sockaddr_in *)addr)->sin_addr);
 		return;
 	case AF_INET6:
-		/* workaround for ip4 client addr being set to INET6 family.
-		 * This should fix it:
-		 * yotamke@mellanox.com: [PATCH for-next] RDMA/CMA: Mark
-		 * IPv4 addresses correctly when the listener is IPv6]
-		 * http://permalink.gmane.org/gmane.linux.drivers.rdma/22395
-		 *
-		 * The first byte of ip6 address can't be 0. If it is, assume
-		 * structure addr actually contains ip4 address.
-		 */
-		if (!((struct sockaddr_in6 *)addr)->sin6_addr.s6_addr[0]) {
-			scnprintf(buf, len, "ip:%pI4",
-				  &((struct sockaddr_in *)addr)->sin_addr);
-			return;
-		}
-		/* end of workaround*/
 		scnprintf(buf, len, "ip:%pI6c",
 			  &((struct sockaddr_in6 *)addr)->sin6_addr);
 		return;
