63204125aaa9a4b9d5d5fd4676888aee8b7f051a
Danil Kipnis <danil.kipnis@profitbricks.com>
2017-10-27 13:20:15 +0200

    ibnbd: always print session name in log
    Signed-off-by: Danil Kipnis <danil.kipnis@profitbricks.com>

--- drivers/block/ibnbd/ibnbd-log.h
+++ drivers/block/ibnbd/ibnbd-log.h
@@ -9,9 +9,6 @@
 	gd ? gd->disk_name : "<no dev>";				\
 })
 
-#define ibnbd_prefix(dev) ((dev)->sess->hostname[0] ? (dev)->sess->hostname : \
-			   (dev)->sess->str_addr)
-
 void unknown_type(void);
 
 #define ibnbd_log(fn, dev, fmt, ...) ({					\
@@ -19,13 +16,13 @@ void unknown_type(void);
 		__builtin_types_compatible_p(				\
 			typeof(dev), struct ibnbd_clt_dev *),		\
 		fn("<%s@%s> %s: " fmt, (dev)->pathname,		\
-		   ibnbd_prefix(dev), ibnbd_diskname(dev),		\
+		   (dev)->sess->sessname, ibnbd_diskname(dev),		\
 		   ##__VA_ARGS__),					\
 		__builtin_choose_expr(					\
 			__builtin_types_compatible_p(typeof(dev),	\
 					struct ibnbd_srv_sess_dev *),	\
 			fn("<%s@%s>: " fmt, (dev)->pathname,	\
-			   ibnbd_prefix(dev), ##__VA_ARGS__),		\
+			   (dev)->sess->sessname, ##__VA_ARGS__),		\
 			unknown_type()));				\
 })
 
