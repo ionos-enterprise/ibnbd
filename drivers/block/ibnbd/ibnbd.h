d2367c6d5313b85be697c1d984822c70654509c7
Danil Kipnis <danil.kipnis@profitbricks.com>
2017-10-30 13:17:11 +0100

    ibnbd: remove sockaddr_to_str not used any more
    Signed-off-by: Danil Kipnis <danil.kipnis@profitbricks.com>

--- drivers/block/ibnbd/ibnbd.h
+++ drivers/block/ibnbd/ibnbd.h
@@ -60,25 +60,6 @@
 /* TODO: should be configurable */
 #define IBTRS_PORT 1234
 
-static inline int ibnbd_sockaddr_to_str(const struct sockaddr *addr,
-					char *buf, size_t len)
-{
-	switch (addr->sa_family) {
-	case AF_IB:
-		return scnprintf(buf, len, "gid:%pI6",
-				 &((struct sockaddr_ib *)addr)->sib_addr.sib_raw);
-	case AF_INET:
-		return scnprintf(buf, len, "ip:%pI4",
-				 &((struct sockaddr_in *)addr)->sin_addr);
-	case AF_INET6:
-		return scnprintf(buf, len, "ip:%pI6c",
-				 &((struct sockaddr_in6 *)addr)->sin6_addr);
-	default:
-		pr_err("Invalid address family\n");
-		return -EINVAL;
-	}
-}
-
 u32 rq_cmd_to_ibnbd_io_flags(struct request *rq);
 u32 ibnbd_io_flags_to_bi_rw(u32 flags);
 
