b455be1d25af3a588b8548a468218cffd6e62604
Danil Kipnis <danil.kipnis@profitbricks.com>
2017-11-27 14:28:24 +0100

    ibnbd: get rid of ibnbd-proto.c
    Signed-off-by: Danil Kipnis <danil.kipnis@profitbricks.com>

--- drivers/block/ibnbd/ibnbd.c
+++ drivers/block/ibnbd/ibnbd.c
@@ -106,3 +106,33 @@ u32 rq_to_ibnbd_flags(struct request *rq)
 	return result;
 }
 EXPORT_SYMBOL_GPL(rq_to_ibnbd_flags);
+
+const char *ibnbd_io_mode_str(enum ibnbd_io_mode mode)
+{
+	switch (mode) {
+	case IBNBD_FILEIO:
+		return "fileio";
+	case IBNBD_BLOCKIO:
+		return "blockio";
+	case IBNBD_AUTOIO:
+		return "autoio";
+	default:
+		return "unknown";
+	}
+}
+EXPORT_SYMBOL_GPL(ibnbd_io_mode_str);
+
+const char *ibnbd_access_mode_str(enum ibnbd_access_mode mode)
+{
+	switch (mode) {
+	case IBNBD_ACCESS_RO:
+		return "ro";
+	case IBNBD_ACCESS_RW:
+		return "rw";
+	case IBNBD_ACCESS_MIGRATION:
+		return "migration";
+	default:
+		return "unknown";
+	}
+}
+EXPORT_SYMBOL_GPL(ibnbd_access_mode_str);
