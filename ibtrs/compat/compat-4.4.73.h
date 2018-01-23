4a2f2ca80fe310314065b28b553e442eb181be4a
Roman Pen <roman.penyaev@profitbricks.com>
2018-01-23 16:44:44 +0100

    IBNBD,IBTRS: license tweaks for each file
    1. Make license shorter.  Giving a link on GPL2 is enough.
    2. Provide missing license info for ibtrs.h and compat files.
    3. Specify authors for ibnbd/itrs 2 iteration 2017-2018 year.

--- ibtrs/compat/compat-4.4.73.h
+++ ibtrs/compat/compat-4.4.73.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) Roman Pen, ProfitBricks GmbH.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef IBTRS_4_4_73_COMPAT_H
 #define IBTRS_4_4_73_COMPAT_H
 
