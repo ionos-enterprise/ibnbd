d5b17dc2c2feb5f7ae0b8d7206c32a49a6a9361e
Roman Pen <roman.penyaev@profitbricks.com>
2018-04-23 17:28:55 +0200

    rculist: introduce list_next_or_null_rr_rcu()
    Function is going to be used in transport over RDMA module
    in subsequent patches.
    
    Function returns next element in round-robin fashion,
    i.e. head will be skipped.  NULL will be returned if list
    is observed as empty.
    
    Signed-off-by: Roman Pen <roman.penyaev@profitbricks.com>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: linux-kernel@vger.kernel.org

--- include/linux/rculist.h
+++ include/linux/rculist.h
@@ -339,6 +339,25 @@ static inline void list_splice_tail_init_rcu(struct list_head *list,
 })
 
 /**
+ * list_next_or_null_rr_rcu - get next list element in round-robin fashion.
+ * @head:	the head for the list.
+ * @ptr:        the list head to take the next element from.
+ * @type:       the type of the struct this is embedded in.
+ * @memb:       the name of the list_head within the struct.
+ *
+ * Next element returned in round-robin fashion, i.e. head will be skipped,
+ * but if list is observed as empty, NULL will be returned.
+ *
+ * This primitive may safely run concurrently with the _rcu list-mutation
+ * primitives such as list_add_rcu() as long as it's guarded by rcu_read_lock().
+ */
+#define list_next_or_null_rr_rcu(head, ptr, type, memb) \
+({ \
+	list_next_or_null_rcu(head, ptr, type, memb) ?: \
+		list_next_or_null_rcu(head, READ_ONCE((ptr)->next), type, memb); \
+})
+
+/**
  * list_for_each_entry_rcu	-	iterate over rcu list of given type
  * @pos:	the type * to use as a loop cursor.
  * @head:	the head for your list.
