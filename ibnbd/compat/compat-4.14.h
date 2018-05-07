cbc6bfd250325add0cc05e204914bc7b8c32d573
Roman Pen <roman.penyaev@profitbricks.com>
2018-05-07 13:33:00 +0200

    COMPAT: IBNBD: sysfs_remove_file_self()

--- ibnbd/compat/compat-4.14.h
+++ ibnbd/compat/compat-4.14.h
@@ -18,6 +18,33 @@
 #ifndef LINUX_4_14_COMPAT_H
 #define LINUX_4_14_COMPAT_H
 
+/*
+ * linux/sysfs.h
+ */
+#define sysfs_remove_file_self ORIGINAL_sysfs_remove_file_self
+#include <linux/sysfs.h>
+#include <linux/device.h>
+#undef sysfs_remove_file_self
+
+static inline
+void sysfs_remove_file_self(struct kobject *kobj,
+			    const struct attribute *attr)
+{
+       struct device_attribute dattr = {
+               .attr.name = attr->name
+       };
+       struct device *device;
+
+       /*
+        * Unfortunately original sysfs_remove_file_self() is not exported,
+        * so consider this as a hack to call self removal of a sysfs entry
+        * just using another "door".
+        */
+
+       device = container_of(kobj, typeof(*device), kobj);
+       device_remove_file_self(device, &dattr);
+}
+
 #include <linux/version.h>
 #include <linux/blk-mq.h>
 
