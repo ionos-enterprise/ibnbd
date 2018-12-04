a5366223f3c6ab9f7e848b01f424f149085a08cf
Danil Kipnis <danil.kipnis@profitbricks.com>
2018-12-04 16:17:35 +0100

    ibnbd-clt: log don't ever access gendisk
    ibnbd_info macro accesses gd->disk_name. It can be used in code after
    the gendisk is deleted and putted in destroy_gen_disk() here in
    ibnbd-clt.c:
    1699         destroy_gen_disk(dev);
    1700         if (was_mapped && sess->ibtrs)
    1701                 send_msg_close(dev, dev->device_id, WAIT);
    1702
    1703         ibnbd_info(dev, "Device is unmapped\n");
    
    Don't ever access gendisk structure
    
    Signed-off-by: Danil Kipnis <danil.kipnis@profitbricks.com>

--- ibnbd/ibnbd-log.h
+++ ibnbd/ibnbd-log.h
@@ -33,19 +33,14 @@
 #include "ibnbd-clt.h"
 #include "ibnbd-srv.h"
 
-#define ibnbd_diskname(dev) ({						\
-	struct gendisk *gd = ((struct ibnbd_clt_dev *)dev)->gd;		\
-	gd ? gd->disk_name : "<no dev>";				\
-})
-
 void unknown_type(void);
 
 #define ibnbd_log(fn, dev, fmt, ...) ({					\
 	__builtin_choose_expr(						\
 		__builtin_types_compatible_p(				\
 			typeof(dev), struct ibnbd_clt_dev *),		\
-		fn("<%s@%s> %s: " fmt, (dev)->pathname,		\
-		   (dev)->sess->sessname, ibnbd_diskname(dev),		\
+		fn("<%s@%s> " fmt, (dev)->pathname,		\
+		   (dev)->sess->sessname, 		\
 		   ##__VA_ARGS__),					\
 		__builtin_choose_expr(					\
 			__builtin_types_compatible_p(typeof(dev),	\
