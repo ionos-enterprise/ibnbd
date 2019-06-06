0a4e91f459597e53d4157c954347aa7d7e5a8694
Danil Kipnis <danil.kipnis@profitbricks.com>
2019-06-06 14:53:42 +0200

    ibnbd: replace GPL test by SPDX thing
    Remove the GPL text with
    sed -i -e '/This program/,/along with this/d' $i
    
    Add the SPDX thing at the beginning
    
    Remove authors from the compat headers - roman wrote it alone
    
    Signed-off-by: Danil Kipnis <danil.kipnis@profitbricks.com>

--- ibnbd/ibnbd-srv.h
+++ ibnbd/ibnbd-srv.h
@@ -1,3 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
  * InfiniBand Network Block Driver
  *
@@ -14,22 +15,9 @@
  *          Roman Penyaev <roman.penyaev@profitbricks.com>
  *
  * Copyright (c) 2018 - 2019 1&1 IONOS Cloud GmbH. All rights reserved.
- * Authors: Roman Penyaev <rpenyaev@suse.com>
+ * Authors: Roman Penyaev <roman.penyaev@profitbricks.com>
  *          Jinpu Wang <jinpu.wang@cloud.ionos.com>
  *          Danil Kipnis <danil.kipnis@cloud.ionos.com>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef IBNBD_SRV_H
